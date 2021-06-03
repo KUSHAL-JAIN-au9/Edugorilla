2c39b56194d80602c24c0cf82854f514f57940b9
Kushal-Jain-au8 <jkushal069@gmail.com>
2021-06-03 18:02:57 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Quiz from "./pages/Quiz";
+import QuizSummary from "./pages/QuizSummary";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  const titleContent = "ini adalah title";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Quiz} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/about" component={Home} />
+          <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/summary" component={QuizSummary} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/quiz/fakeData.js
@@ -0,0 +1,519 @@
+export const quiz = [
+  {
+    id: 1,
+    question: "Which of the following is correct about JavaScript?",
+    options: [
+      {
+        title: "JavaScript is a lightweight, interpreted programming language.",
+        correct: false,
+      },
+      {
+        title: "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages.",
+        correct: false,
+      },
+      {
+        title: "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers.",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Which of the following is true about cookie handling in JavaScript?",
+    options: [
+      {
+        title: "JavaScript can manipulate cookies using the cookie property of the Document object.",
+        correct: false,
+      },
+    
+      {
+        title: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+        correct: false,
+      },
+      {
+        title: "Both of the above",
+        correct: true,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+    options: [
+      {
+        title: "last()",
+        correct: false,
+      },
+      {
+        title: "put()",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+      {
+        title: "push()",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which of the following function of Number object formats a number with a specific number of digits to the right of the decimal?",
+    options: [
+      {
+        title: "toExponential()",
+        correct: false,
+      },
+      {
+        title: "toFixed()",
+        correct: true,
+      },
+      {
+        title: "toPrecision()",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: " In which of the following areas, Node.js is perfect to use?",
+    options: [
+      {
+        title: " I/O bound Applications",
+        correct: false,
+      },
+      {
+        title: "Data Streaming Applications",
+        correct: false,
+      },
+      {
+        title: "Data Intensive Realtime Applications (DIRT)",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Which of the following is true about package.json?",
+    options: [
+      {
+        title: "package.json is present in the root directory of any Node application/module.",
+        correct: false,
+      },
+      {
+        title: "package.json is used to define the properties of a package.",
+        correct: false,
+      },
+      {
+        title: "package.json can be used to update dependencies of a Node application.",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: " Which of the following is true about readable stream?",
+    options: [
+      {
+        title: "Readable stream is used for read operation.",
+        correct: false,
+      },
+      {
+        title: "Output of readable stream can be input to a writable stream.",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+      {
+        title: "Both of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which method of fs module is used to close a file?t",
+    options: [
+      {
+        title: "fs.closeFile(fd, callback)",
+        correct: false,
+      },
+      {
+        title: " fs.close(fd, callback)",
+        correct: true,
+      },
+      {
+        title: "fs.closePath(fd, callback).",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: "How can you access the state of a component from inside of a member function? ",
+    options: [
+      {
+        title: "this.values",
+        correct: true,
+      },
+      {
+        title: "this.getState()",
+        correct: false,
+      },
+      {
+        title: "this.prototype.stateValue",
+        correct: false,
+      },
+      {
+        title: "this.state",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "At the highest level, React components have lifecycle events that fall into ",
+    options: [
+      {
+        title: "Initialization",
+        correct: false,
+      },
+      {
+        title: "State/Property Updates",
+        correct:false,
+      },
+    
+      {
+        title: "Destruction",
+        correct: false,
+      },
+      {
+        title: "All of these",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: " What kind of component import React from 'react' is? ",
+    options: [
+      {
+        title: "stateless component",
+        correct: true,
+      },
+      {
+        title: "statefull component",
+        correct: false,
+      },
+      {
+        title: "pure component",
+        correct: false,
+      },
+      {
+        title: "class component",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is ReactJS? ",
+    options: [
+      {
+        title: "Server-side Framework",
+        correct: false,
+      },
+      {
+        title: "User-interface framework",
+        correct: true,
+      },
+      {
+        title: "Both server & Client",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: "Which of the following is correct about JavaScript?",
+    options: [
+      {
+        title: "JavaScript is a lightweight, interpreted programming language.",
+        correct: false,
+      },
+      {
+        title: "JavaScript has object-oriented capabilities that allows you to build interactivity into otherwise static HTML pages.",
+        correct: false,
+      },
+      {
+        title: "The general-purpose core of the language has been embedded in Netscape, Internet Explorer, and other web browsers.",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Which of the following is true about cookie handling in JavaScript?",
+    options: [
+      {
+        title: "JavaScript can manipulate cookies using the cookie property of the Document object.",
+        correct: false,
+      },
+   
+      {
+        title: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+        correct: false,
+      },
+      {
+        title: "Both of the above",
+        correct: true,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+    options: [
+      {
+        title: "last()",
+        correct: false,
+      },
+      {
+        title: "put()",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+      {
+        title: "push()",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which of the following function of Number object formats a number with a specific number of digits to the right of the decimal?",
+    options: [
+      {
+        title: "toExponential()",
+        correct: false,
+      },
+      {
+        title: "toFixed()",
+        correct: true,
+      },
+      {
+        title: "toPrecision()",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: " In which of the following areas, Node.js is perfect to use?",
+    options: [
+      {
+        title: " I/O bound Applications",
+        correct: false,
+      },
+      {
+        title: "Data Streaming Applications",
+        correct: false,
+      },
+      {
+        title: "Data Intensive Realtime Applications (DIRT)",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Which of the following is true about package.json?",
+    options: [
+      {
+        title: "package.json is present in the root directory of any Node application/module.",
+        correct: false,
+      },
+      {
+        title: "package.json is used to define the properties of a package.",
+        correct: false,
+      },
+      {
+        title: "package.json can be used to update dependencies of a Node application.",
+        correct: false,
+      },
+      {
+        title: "All of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: " Which of the following is true about readable stream?",
+    options: [
+      {
+        title: "Readable stream is used for read operation.",
+        correct: false,
+      },
+      {
+        title: "Output of readable stream can be input to a writable stream.",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+      {
+        title: "Both of the above.",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which method of fs module is used to close a file?t",
+    options: [
+      {
+        title: "fs.closeFile(fd, callback)",
+        correct: false,
+      },
+      {
+        title: " fs.close(fd, callback)",
+        correct: true,
+      },
+      {
+        title: "fs.closePath(fd, callback).",
+        correct: false,
+      },
+      {
+        title: "None of the above.",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: "How can you access the state of a component from inside of a member function? ",
+    options: [
+      {
+        title: "this.values",
+        correct: true,
+      },
+      {
+        title: "this.getState()",
+        correct: false,
+      },
+      {
+        title: "this.prototype.stateValue",
+        correct: false,
+      },
+      {
+        title: "this.state",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "At the highest level, React components have lifecycle events that fall into ",
+    options: [
+      {
+        title: "Initialization",
+        correct: false,
+      },
+      {
+        title: "State/Property Updates",
+        correct:false,
+      },
+    
+      {
+        title: "Destruction",
+        correct: false,
+      },
+      {
+        title: "All of these",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: " What kind of component import React from 'react' is? ",
+    options: [
+      {
+        title: "stateless component",
+        correct: true,
+      },
+      {
+        title: "statefull component",
+        correct: false,
+      },
+      {
+        title: "pure component",
+        correct: false,
+      },
+      {
+        title: "class component",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is ReactJS? ",
+    options: [
+      {
+        title: "Server-side Framework",
+        correct: false,
+      },
+      {
+        title: "User-interface framework",
+        correct: true,
+      },
+      {
+        title: "Both server & Client",
+        correct: false,
+      },
+    ],
+  },
+
+];
